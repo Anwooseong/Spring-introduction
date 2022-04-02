dc5433dbad4fbbb040f89aed1d56609215736afa
Anwooseong <aws5624@naver.com>
2022-04-02 17:22:03 +0900

    스프링 데이터 JPA 구현

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -4,6 +4,7 @@ import hello.hellospring.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,7 +15,7 @@ class MemberServiceIntegrationTest {
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
     @Test
-    public void 회원가입() throws Exception {
+    void 회원가입() throws Exception {
         //Given
         Member member = new Member();
         member.setName("hello");
@@ -25,7 +26,7 @@ class MemberServiceIntegrationTest {
         assertEquals(member.getName(), findMember.getName());
     }
     @Test
-    public void 중복_회원_예외() throws Exception {
+    void 중복_회원_예외() throws Exception {
         //Given
         Member member1 = new Member();
         member1.setName("spring");
